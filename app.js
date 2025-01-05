0268143146459c68fd9d5d8c5a7d3d3a7382b9eb
Truth <88577503+Irehh@users.noreply.github.com>
2025-01-05 16:52:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use('/api/users/', router)
 app.use('/api/blog/', blogRouter)
 
-mongoose.connect('mongodb+srv://truth:truth@firstcluster.8lde0.mongodb.net/?retryWrites=true&w=majority&appName=Firstcluster').then(()=>app.listen(5000)).then(()=>console.log('Connected to first cluster and listening on port 5000')).catch((err) => console.log(err));
+mongoose.connect('connect to mongo here').then(()=>app.listen(5000)).then(()=>console.log('Connected to first cluster and listening on port 5000')).catch((err) => console.log(err));
